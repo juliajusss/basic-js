331ddec5c89bf2d40daed8d07424885ff01e6ff7
Yuliya Osmolovskaya <yuliya.asmalouskaya@gmail.com>
2020-03-22 00:34:49 +0100

    feat: add solution to extended-repeater and vigenere-cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,27 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports =
+    function repeater(str, options) {
+        let repeatTimes = options.repeatTimes !== undefined ? options.repeatTimes : 1;
+        let separator = options.separator !== undefined ? options.separator : '+';
+        let addition = options.addition !== undefined ? options.addition : '';
+        let additionRepeatTimes = options.additionRepeatTimes !== undefined ? options.additionRepeatTimes : 1;
+        let additionSeparator = options.additionSeparator !== undefined ? options.additionSeparator : '|';
+
+        let newStr = '';
+
+        for (let i = 0; i < repeatTimes; i++) {
+            if (i !== 0) {
+                newStr += separator;
+            }
+            newStr += String(str);
+            for (let k = 0; k < additionRepeatTimes; k++) {
+                if (k !== 0) {
+                    newStr += additionSeparator;
+                }
+                newStr += addition;
+            }
+        }
+
+        return newStr;
+    };
+
+
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,87 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    constructor(direct) {
+        this.direct = direct;
+        if (direct === undefined) {
+            this.direct = true;
+        }
+        this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if (!message || !key) {
+            throw "Error!"
+        }
+
+
+        let result = '';
+        message = message.toUpperCase();
+
+        key = this.keyMaker(key, message);
+
+        let alphabetArray = this.alphabet.split('');
+        for (let i = 0; i < message.length; i++) {
+            if (!(/[A-Z]/.test(message[i]))) {
+                result += message[i];
+            } else {
+                let finalIndex = alphabetArray.indexOf(message[i]) + alphabetArray.indexOf(key[i]);
+                if (finalIndex > 25) {
+                    finalIndex = finalIndex - 26;
+                }
+                result += alphabetArray[finalIndex];
+            }
+        }
+        if (this.direct) {
+            return result;
+        } else {
+            return result.split('').reverse().join('');
+        }
     }
+
+    decrypt(encryptedMessage, key) {
+
+        let result = '';
+
+        key = this.keyMaker(key, encryptedMessage);
+
+        let alphabetArray = this.alphabet.split('');
+        for (let i = 0; i < encryptedMessage.length; i++) {
+            if (!(/[A-Z]/.test(encryptedMessage[i]))) {
+                result += encryptedMessage[i];
+            } else {
+                let finalIndex = alphabetArray.indexOf(encryptedMessage[i]) - alphabetArray.indexOf(key[i]);
+                if (finalIndex < 0) {
+                    finalIndex = finalIndex + 26;
+                }
+                result += alphabetArray[finalIndex];
+            }
+        }
+
+        if (this.direct) {
+            return result;
+        } else {
+            return result.split('').reverse().join('');
+        }
+
+    }
+
+    keyMaker(key, message) {
+        key = key.toUpperCase();
+
+        if (message.length > key.length) {
+            key = key.repeat(message.length).split('');
+            for (let i = 0; i < message.length; i++) {
+                if (message[i] === ' ') {
+                    key.splice(i, 0, ' ');
+                }
+            }
+            key = key.join('').substring(0, message.length);
+        }
+        return key;
+    }
+
 }
 
 module.exports = VigenereCipheringMachine;
+
+
