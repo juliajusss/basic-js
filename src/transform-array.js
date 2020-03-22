8665d0e9dbe0096e678c1493572245fd450fdfbb
Yuliya Osmolovskaya <yuliya.asmalouskaya@gmail.com>
2020-03-22 21:48:13 +0100

    feat:add solution to transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,31 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports =
+    function transform(arr) {
+        if (!Array.isArray(arr)) {
+            throw "Error";
+        }
+
+        let sequences = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+        let result = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === sequences[0]) {
+                i++;
+            } else if (arr[i] === sequences[1]) {
+                result.pop();
+            } else if (arr[i] === sequences[2]) {
+                if (arr.length - 1 > i) {
+                    result.push(arr[i + 1]);
+                }
+            } else if (arr[i] === sequences[3]) {
+                if (i > 0) {
+                    result.push(arr[i - 1]);
+                }
+            } else {
+                result.push(arr[i]);
+            }
+
+        }
+
+        return result;
+    };
+
